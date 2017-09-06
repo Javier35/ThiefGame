8a569347556808c621490971de4cc00745043caa
Javier <35javier35@gmail.com>
2017-09-05 18:25:12 -0600

    Progress on player following
    Follower now follows the player, flipping directions is missing though, and i need to distance the follower so it doesnt overlap with the player

---
+++ Assets/AnimationNameTranslator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationNameTranslator : MonoBehaviour {
+
+	Dictionary<int, string> animationHash = new Dictionary<int, string>();
+
+	void Start(){
+
+		animationHash.Add(Animator.StringToHash ("Idle") , "Idle");
+		animationHash.Add(Animator.StringToHash ("Run") , "Run");
+		animationHash.Add(Animator.StringToHash ("Dash") , "Dash");
+		animationHash.Add(Animator.StringToHash ("Jump") , "Jump");
+		animationHash.Add(Animator.StringToHash ("Fall") , "Fall");
+		animationHash.Add(Animator.StringToHash ("Death") , "Death");
+	}
+
+	public string getNameByHash(int hash){
+
+		if (animationHash.ContainsKey(hash))
+		{
+			return animationHash[hash];
+		}
+
+		return "Idle";
+	}
+}
---
+++ Assets/FollowerBehavior.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowerBehavior : MonoBehaviour {
+
+
+	//Target components
+	List<Vector3> positionsList = new List<Vector3>();
+	List<string> animationNamesList = new List<string>();
+
+
+	public GameObject targetObject;
+	private Transform targetTransform;
+	private Animator targetAnimator;
+
+
+	//My components
+	AnimationNameTranslator hashTranslator;
+	public float nonFollowDistance = 1f;
+	public float followSpeed = 2.4f;
+	bool followEnabled = false;
+	private Animator anim;
+
+
+	void Start () {
+		targetTransform = targetObject.GetComponent<Transform> ();
+		targetAnimator = targetObject.GetComponent<Animator> ();
+
+		hashTranslator = GetComponent<AnimationNameTranslator> ();
+		anim = GetComponent<Animator> ();
+
+		Invoke ("EnableFollow", 0.48f);
+	}
+
+	void Update () {
+
+		if (followEnabled) {
+			transform.position = Vector3.Lerp (transform.position, positionsList[0], followSpeed);
+			positionsList.RemoveAt (0);
+
+			anim.Play (animationNamesList[0]);
+			animationNamesList.RemoveAt (0);
+
+		}
+		positionsList.Add (targetTransform.position);
+
+
+		var currentAnimHash = targetAnimator.GetCurrentAnimatorStateInfo (0).shortNameHash;
+		var currentAnimName = hashTranslator.getNameByHash (currentAnimHash);
+		animationNamesList.Add (currentAnimName);
+	}
+
+	void EnableFollow(){
+		followEnabled = true;
+	}
+	void DisableFollow(){
+		followEnabled = false;
+	}
+}
